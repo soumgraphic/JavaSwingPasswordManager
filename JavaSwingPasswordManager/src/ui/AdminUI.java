abebdbad45684aaba5b529c952fb98304f87b842
soumgraphic <soumgraphic@gmail.com>
2018-12-23 06:56:35 +0100

    MAJ UI Admin Fin de la version 1.

--- JavaSwingPasswordManager/src/ui/AdminUI.java
+++ JavaSwingPasswordManager/src/ui/AdminUI.java
@@ -18,6 +18,8 @@ import javax.swing.JSeparator;
 import javax.swing.SwingConstants;
 import java.awt.Cursor;
 import javax.swing.JScrollBar;
+import javax.swing.JPasswordField;
+import javax.swing.JButton;
 
 public class AdminUI {
 
@@ -25,6 +27,16 @@ public class AdminUI {
 	JLabel userNameLbl;
 	JLabel userEmailLbl;
 	private final JPanel panel = new JPanel();
+	private JTextField passwordItemTxtFld;
+	private JTextField usernameItemTxtFld;
+	private JTextField urlItemTxtFld;
+	private JTextField nomCompletItemTxtFld;
+	private JTextField etatPasswordItemTxtFld;
+	private JTextField etatUrlItemTxtFld;
+	private JTextField nomCompletEditTxtFld;
+	private JTextField usernameEditTxtFld;
+	private JPasswordField passwordEditTxtFld;
+	private JTextField urlEditTxtFld;
 
 	/**
 	 * Launch the application.
@@ -67,8 +79,8 @@ public class AdminUI {
 		frame.setBounds(100, 100, 1200, 650);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
-		panel.setBackground(new Color(255, 255, 240));
-		panel.setBounds(0, 8, 1194, 150);
+		panel.setBackground(new Color(255, 255, 255));
+		panel.setBounds(0, 8, 1200, 150);
 		frame.getContentPane().add(panel);
 		panel.setLayout(null);
 		
@@ -130,7 +142,7 @@ public class AdminUI {
 		panel.add(lblNewLabel_3);
 		
 		JSeparator separator_4 = new JSeparator();
-		separator_4.setBounds(208, 71, 688, 2);
+		separator_4.setBounds(214, 61, 688, 12);
 		panel.add(separator_4);
 		
 		JLabel scoreSecuriteLbl = new JLabel("Score de sécurité");
@@ -162,45 +174,45 @@ public class AdminUI {
 		separator_6.setBounds(790, 71, 10, 78);
 		panel.add(separator_6);
 		
-		JLabel lblNombreDeCompte = new JLabel("Nombre de compte enregistré");
-		lblNombreDeCompte.setForeground(new Color(112, 128, 144));
-		lblNombreDeCompte.setFont(new Font("Tahoma", Font.PLAIN, 14));
-		lblNombreDeCompte.setBounds(450, 86, 183, 17);
-		panel.add(lblNombreDeCompte);
+		JLabel nbreCompteEnregistrerLbl = new JLabel("Nombre de compte enregistré");
+		nbreCompteEnregistrerLbl.setForeground(new Color(112, 128, 144));
+		nbreCompteEnregistrerLbl.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		nbreCompteEnregistrerLbl.setBounds(450, 86, 183, 17);
+		panel.add(nbreCompteEnregistrerLbl);
 		
-		JLabel label_2 = new JLabel("10");
-		label_2.setForeground(new Color(112, 128, 144));
-		label_2.setFont(new Font("Tahoma", Font.BOLD, 18));
-		label_2.setBounds(519, 107, 44, 22);
-		panel.add(label_2);
+		JLabel chiffreNbreCompteEnregistrerLbl = new JLabel("10");
+		chiffreNbreCompteEnregistrerLbl.setForeground(new Color(112, 128, 144));
+		chiffreNbreCompteEnregistrerLbl.setFont(new Font("Tahoma", Font.BOLD, 18));
+		chiffreNbreCompteEnregistrerLbl.setBounds(519, 107, 44, 22);
+		panel.add(chiffreNbreCompteEnregistrerLbl);
 		
 		JLabel lblNewLabel_2 = new JLabel("");
 		lblNewLabel_2.setBackground(new Color(60, 179, 113));
 		lblNewLabel_2.setOpaque(true);
-		lblNewLabel_2.setBounds(0, 0, 1194, 8);
+		lblNewLabel_2.setBounds(0, 0, 1200, 8);
 		frame.getContentPane().add(lblNewLabel_2);
 		
 		JPanel panel_1 = new JPanel();
 		panel_1.setLayout(null);
-		panel_1.setBackground(new Color(255, 255, 240));
-		panel_1.setBounds(0, 179, 792, 423);
+		panel_1.setBackground(new Color(255, 255, 255));
+		panel_1.setBounds(0, 179, 792, 432);
 		frame.getContentPane().add(panel_1);
 		
 		JSeparator separator_3 = new JSeparator();
 		separator_3.setBounds(0, 58, 792, 12);
 		panel_1.add(separator_3);
 		
-		JLabel lblNewLabel = new JLabel("");
-		lblNewLabel.setIcon(new ImageIcon(AdminUI.class.getResource("/ui/images/lock_pass.png")));
-		lblNewLabel.setBounds(12, 13, 32, 32);
-		panel_1.add(lblNewLabel);
+		JLabel iconeTextCompteEnregistrerLbl = new JLabel("");
+		iconeTextCompteEnregistrerLbl.setIcon(new ImageIcon(AdminUI.class.getResource("/ui/images/lock_pass.png")));
+		iconeTextCompteEnregistrerLbl.setBounds(12, 13, 32, 32);
+		panel_1.add(iconeTextCompteEnregistrerLbl);
 		
-		JLabel lblNewLabel_1 = new JLabel("Compte enregistré");
-		lblNewLabel_1.setFont(new Font("Tahoma", Font.BOLD, 17));
-		lblNewLabel_1.setForeground(new Color(112, 128, 144));
-		lblNewLabel_1.setHorizontalAlignment(SwingConstants.CENTER);
-		lblNewLabel_1.setBounds(56, 18, 167, 22);
-		panel_1.add(lblNewLabel_1);
+		JLabel compteEnregistrerLbl = new JLabel("Compte enregistré");
+		compteEnregistrerLbl.setFont(new Font("Tahoma", Font.BOLD, 17));
+		compteEnregistrerLbl.setForeground(new Color(112, 128, 144));
+		compteEnregistrerLbl.setHorizontalAlignment(SwingConstants.CENTER);
+		compteEnregistrerLbl.setBounds(56, 18, 167, 22);
+		panel_1.add(compteEnregistrerLbl);
 		
 		JLabel lblCompte = new JLabel("Compte");
 		lblCompte.setFont(new Font("Tahoma", Font.BOLD, 14));
@@ -211,13 +223,13 @@ public class AdminUI {
 		JLabel lblMotDePasse = new JLabel("Mot de passe");
 		lblMotDePasse.setForeground(new Color(112, 128, 144));
 		lblMotDePasse.setFont(new Font("Tahoma", Font.BOLD, 14));
-		lblMotDePasse.setBounds(246, 73, 91, 17);
+		lblMotDePasse.setBounds(246, 73, 92, 17);
 		panel_1.add(lblMotDePasse);
 		
 		JLabel lblUrl = new JLabel("URL");
 		lblUrl.setForeground(new Color(112, 128, 144));
 		lblUrl.setFont(new Font("Tahoma", Font.BOLD, 14));
-		lblUrl.setBounds(500, 73, 91, 17);
+		lblUrl.setBounds(470, 73, 91, 17);
 		panel_1.add(lblUrl);
 		
 		JSeparator separator_7 = new JSeparator();
@@ -225,8 +237,105 @@ public class AdminUI {
 		panel_1.add(separator_7);
 		
 		JScrollBar scrollBar = new JScrollBar();
-		scrollBar.setBounds(764, 114, 16, 296);
+		scrollBar.setBackground(new Color(255, 255, 255));
+		scrollBar.setBounds(764, 125, 16, 301);
 		panel_1.add(scrollBar);
+		
+		JLabel lblAction = new JLabel("Action");
+		lblAction.setForeground(new Color(112, 128, 144));
+		lblAction.setFont(new Font("Tahoma", Font.BOLD, 14));
+		lblAction.setBounds(655, 72, 45, 17);
+		panel_1.add(lblAction);
+		
+		JLabel iconViewOrHidePasswordItemLbl = new JLabel("");
+		iconViewOrHidePasswordItemLbl.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+		iconViewOrHidePasswordItemLbl.setIcon(new ImageIcon(AdminUI.class.getResource("/ui/images/eye.png")));
+		iconViewOrHidePasswordItemLbl.setForeground(new Color(112, 128, 144));
+		iconViewOrHidePasswordItemLbl.setFont(new Font("Tahoma", Font.BOLD, 16));
+		iconViewOrHidePasswordItemLbl.setBounds(408, 125, 23, 23);
+		panel_1.add(iconViewOrHidePasswordItemLbl);
+		
+		passwordItemTxtFld = new JTextField();
+		passwordItemTxtFld.setFont(new Font("Tahoma", Font.BOLD, 15));
+		passwordItemTxtFld.setForeground(new Color(112, 128, 144));
+		passwordItemTxtFld.setBorder(null);
+		passwordItemTxtFld.setBackground(new Color(255, 255, 255));
+		passwordItemTxtFld.setEditable(false);
+		passwordItemTxtFld.setText("**********");
+		passwordItemTxtFld.setBounds(246, 125, 150, 22);
+		panel_1.add(passwordItemTxtFld);
+		passwordItemTxtFld.setColumns(10);
+		
+		usernameItemTxtFld = new JTextField();
+		usernameItemTxtFld.setText("soumgraphic@gmail.com");
+		usernameItemTxtFld.setForeground(new Color(112, 128, 144));
+		usernameItemTxtFld.setFont(new Font("Tahoma", Font.BOLD, 15));
+		usernameItemTxtFld.setEditable(false);
+		usernameItemTxtFld.setColumns(10);
+		usernameItemTxtFld.setBorder(null);
+		usernameItemTxtFld.setBackground(Color.WHITE);
+		usernameItemTxtFld.setBounds(12, 125, 211, 22);
+		panel_1.add(usernameItemTxtFld);
+		
+		urlItemTxtFld = new JTextField();
+		urlItemTxtFld.setText("www.youtube.com");
+		urlItemTxtFld.setForeground(new Color(112, 128, 144));
+		urlItemTxtFld.setFont(new Font("Tahoma", Font.BOLD, 15));
+		urlItemTxtFld.setEditable(false);
+		urlItemTxtFld.setColumns(10);
+		urlItemTxtFld.setBorder(null);
+		urlItemTxtFld.setBackground(Color.WHITE);
+		urlItemTxtFld.setBounds(470, 125, 160, 22);
+		panel_1.add(urlItemTxtFld);
+		
+		nomCompletItemTxtFld = new JTextField();
+		nomCompletItemTxtFld.setText("Soumaila Abdoulaye DIARRA");
+		nomCompletItemTxtFld.setForeground(new Color(112, 128, 144));
+		nomCompletItemTxtFld.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		nomCompletItemTxtFld.setEditable(false);
+		nomCompletItemTxtFld.setColumns(10);
+		nomCompletItemTxtFld.setBorder(null);
+		nomCompletItemTxtFld.setBackground(Color.WHITE);
+		nomCompletItemTxtFld.setBounds(12, 151, 211, 22);
+		panel_1.add(nomCompletItemTxtFld);
+		
+		etatPasswordItemTxtFld = new JTextField();
+		etatPasswordItemTxtFld.setText("Faible");
+		etatPasswordItemTxtFld.setForeground(new Color(112, 128, 144));
+		etatPasswordItemTxtFld.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		etatPasswordItemTxtFld.setEditable(false);
+		etatPasswordItemTxtFld.setColumns(10);
+		etatPasswordItemTxtFld.setBorder(null);
+		etatPasswordItemTxtFld.setBackground(Color.WHITE);
+		etatPasswordItemTxtFld.setBounds(246, 151, 150, 22);
+		panel_1.add(etatPasswordItemTxtFld);
+		
+		etatUrlItemTxtFld = new JTextField();
+		etatUrlItemTxtFld.setText("Site fiable");
+		etatUrlItemTxtFld.setForeground(new Color(112, 128, 144));
+		etatUrlItemTxtFld.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		etatUrlItemTxtFld.setEditable(false);
+		etatUrlItemTxtFld.setColumns(10);
+		etatUrlItemTxtFld.setBorder(null);
+		etatUrlItemTxtFld.setBackground(Color.WHITE);
+		etatUrlItemTxtFld.setBounds(470, 151, 150, 22);
+		panel_1.add(etatUrlItemTxtFld);
+		
+		JSeparator separator_8 = new JSeparator();
+		separator_8.setBounds(0, 177, 752, 12);
+		panel_1.add(separator_8);
+		
+		JLabel iconeEditItemLbl = new JLabel("");
+		iconeEditItemLbl.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+		iconeEditItemLbl.setIcon(new ImageIcon(AdminUI.class.getResource("/ui/images/edit_icon.png")));
+		iconeEditItemLbl.setBounds(655, 135, 30, 30);
+		panel_1.add(iconeEditItemLbl);
+		
+		JLabel iconDeleteItemLbl = new JLabel("");
+		iconDeleteItemLbl.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+		iconDeleteItemLbl.setIcon(new ImageIcon(AdminUI.class.getResource("/ui/images/delete_icon.png")));
+		iconDeleteItemLbl.setBounds(704, 135, 30, 30);
+		panel_1.add(iconDeleteItemLbl);
 		
 		JLabel label_6 = new JLabel("");
 		label_6.setOpaque(true);
@@ -237,13 +346,13 @@ public class AdminUI {
 		JLabel label_1 = new JLabel("");
 		label_1.setOpaque(true);
 		label_1.setBackground(new Color(60, 179, 113));
-		label_1.setBounds(797, 171, 397, 8);
+		label_1.setBounds(797, 170, 403, 9);
 		frame.getContentPane().add(label_1);
 		
 		JPanel panel_2 = new JPanel();
 		panel_2.setLayout(null);
-		panel_2.setBackground(new Color(255, 255, 240));
-		panel_2.setBounds(797, 179, 397, 423);
+		panel_2.setBackground(new Color(255, 255, 255));
+		panel_2.setBounds(797, 179, 403, 432);
 		frame.getContentPane().add(panel_2);
 		
 		JSeparator separator_2 = new JSeparator();
@@ -261,5 +370,103 @@ public class AdminUI {
 		lblEditionDeCompte.setFont(new Font("Tahoma", Font.BOLD, 17));
 		lblEditionDeCompte.setBounds(56, 18, 167, 22);
 		panel_2.add(lblEditionDeCompte);
+		
+		JLabel label_7 = new JLabel("Nom");
+		label_7.setForeground(new Color(60, 179, 113));
+		label_7.setFont(new Font("Tahoma", Font.BOLD, 14));
+		label_7.setBounds(12, 70, 282, 16);
+		panel_2.add(label_7);
+		
+		nomCompletEditTxtFld = new JTextField();
+		nomCompletEditTxtFld.setForeground(new Color(60, 179, 113));
+		nomCompletEditTxtFld.setFont(new Font("Tahoma", Font.BOLD, 12));
+		nomCompletEditTxtFld.setColumns(10);
+		nomCompletEditTxtFld.setCaretColor(new Color(60, 179, 113));
+		nomCompletEditTxtFld.setBorder(null);
+		nomCompletEditTxtFld.setBackground(new Color(255, 255, 255));
+		nomCompletEditTxtFld.setBounds(12, 98, 350, 16);
+		panel_2.add(nomCompletEditTxtFld);
+		
+		JSeparator separator_9 = new JSeparator();
+		separator_9.setForeground(new Color(60, 179, 113));
+		separator_9.setBackground(Color.WHITE);
+		separator_9.setBounds(12, 115, 350, 12);
+		panel_2.add(separator_9);
+		
+		JLabel lblUsername = new JLabel("Username");
+		lblUsername.setForeground(new Color(60, 179, 113));
+		lblUsername.setFont(new Font("Tahoma", Font.BOLD, 14));
+		lblUsername.setBounds(12, 139, 282, 16);
+		panel_2.add(lblUsername);
+		
+		usernameEditTxtFld = new JTextField();
+		usernameEditTxtFld.setForeground(new Color(60, 179, 113));
+		usernameEditTxtFld.setFont(new Font("Tahoma", Font.BOLD, 12));
+		usernameEditTxtFld.setColumns(10);
+		usernameEditTxtFld.setCaretColor(new Color(60, 179, 113));
+		usernameEditTxtFld.setBorder(null);
+		usernameEditTxtFld.setBackground(new Color(255, 255, 255));
+		usernameEditTxtFld.setBounds(12, 167, 350, 16);
+		panel_2.add(usernameEditTxtFld);
+		
+		JSeparator separator_10 = new JSeparator();
+		separator_10.setForeground(new Color(60, 179, 113));
+		separator_10.setBackground(Color.WHITE);
+		separator_10.setBounds(12, 184, 350, 12);
+		panel_2.add(separator_10);
+		
+		JLabel label_9 = new JLabel("Mot de passe");
+		label_9.setForeground(new Color(60, 179, 113));
+		label_9.setFont(new Font("Tahoma", Font.BOLD, 14));
+		label_9.setBounds(12, 208, 282, 16);
+		panel_2.add(label_9);
+		
+		passwordEditTxtFld = new JPasswordField();
+		passwordEditTxtFld.setForeground(new Color(60, 179, 113));
+		passwordEditTxtFld.setFont(new Font("Tahoma", Font.BOLD, 12));
+		passwordEditTxtFld.setCaretColor(new Color(60, 179, 113));
+		passwordEditTxtFld.setBorder(null);
+		passwordEditTxtFld.setBackground(new Color(255, 255, 255));
+		passwordEditTxtFld.setBounds(12, 236, 350, 16);
+		panel_2.add(passwordEditTxtFld);
+		
+		JSeparator separator_11 = new JSeparator();
+		separator_11.setForeground(new Color(60, 179, 113));
+		separator_11.setBackground(Color.WHITE);
+		separator_11.setBounds(12, 253, 350, 12);
+		panel_2.add(separator_11);
+		
+		JSeparator separator_12 = new JSeparator();
+		separator_12.setForeground(new Color(60, 179, 113));
+		separator_12.setBackground(Color.WHITE);
+		separator_12.setBounds(12, 322, 350, 12);
+		panel_2.add(separator_12);
+		
+		urlEditTxtFld = new JTextField();
+		urlEditTxtFld.setForeground(new Color(60, 179, 113));
+		urlEditTxtFld.setFont(new Font("Tahoma", Font.BOLD, 12));
+		urlEditTxtFld.setColumns(10);
+		urlEditTxtFld.setCaretColor(new Color(60, 179, 113));
+		urlEditTxtFld.setBorder(null);
+		urlEditTxtFld.setBackground(Color.WHITE);
+		urlEditTxtFld.setBounds(12, 305, 350, 16);
+		panel_2.add(urlEditTxtFld);
+		
+		JLabel lblUrl_1 = new JLabel("Url");
+		lblUrl_1.setForeground(new Color(60, 179, 113));
+		lblUrl_1.setFont(new Font("Tahoma", Font.BOLD, 14));
+		lblUrl_1.setBounds(12, 277, 282, 16);
+		panel_2.add(lblUrl_1);
+		
+		JButton ajouterEditBtn = new JButton("Ajouter");
+		ajouterEditBtn.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+		ajouterEditBtn.setForeground(new Color(60, 179, 113));
+		ajouterEditBtn.setFont(new Font("Tahoma", Font.BOLD, 14));
+		ajouterEditBtn.setFocusPainted(false);
+		ajouterEditBtn.setContentAreaFilled(false);
+		ajouterEditBtn.setBorder(new LineBorder(new Color(60, 179, 113)));
+		ajouterEditBtn.setActionCommand("inscriptionBtn");
+		ajouterEditBtn.setBounds(12, 356, 350, 40);
+		panel_2.add(ajouterEditBtn);
 	}
 }
