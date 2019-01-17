9c95034c15d44c00bafe3be09ed9042a05d4df4f
soumgraphic <soumgraphic@gmail.com>
2019-01-17 02:42:56 +0100

    Mis à jour fichier connection à la base de donnée pour l'exportation de runnable jar.

--- JavaSwingPasswordManager/src/database/DbConnexion.java
+++ JavaSwingPasswordManager/src/database/DbConnexion.java
@@ -27,7 +27,10 @@ public class DbConnexion {
 	}
 
 	public static Connection getConnection() throws SQLException {
-		connection = DriverManager.getConnection("jdbc:sqlite:" + Constants.DB_PATH);
+		//Eclipse test path
+		connection = DriverManager.getConnection("jdbc:sqlite:resource/" + Constants.DB_PATH);
+		//Runnable jar path 
+		//connection = DriverManager.getConnection("jdbc:sqlite::resource:" + Constants.DB_PATH);
 		Utils.getLogger(Level.INFO, "Connection à la base de donnée effectuer avec succès !");
 
 		return connection;
